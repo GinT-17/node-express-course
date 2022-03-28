03a9c3dc675267887ac6e19ce0fbc5dd26489ff6
GinT-17 <laitingying94@gmail.com>
2022-03-28 19:30:12 +0800

    testing

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,1 +1,15 @@
-console.log('Welcome to Node Tutorial')
+const os =  require('os')
+
+//info about the current user
+console.log(os.userInfo())
+
+//methos returns the system uptime in seconds
+console.log(`The system uptime is ${os.uptime()} seconds`)
+
+const currentOS = {
+    name: os.type(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
+}
+
+console.log(currentOS)
